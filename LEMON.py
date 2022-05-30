5f689bd40bbdd056b412434da2025fadf85255a4
XxDevilsCloverxX <silasrodriguez02@gmail.com>
2022-05-30 11:16:35 -0500

    directory update via bash

--- LEMON.py
+++ LEMON.py
@@ -24,7 +24,7 @@ async def on_ready():
 This is a global declaration block of variables that can be used
 during LEMON's runtime...must be updated manually as games are released
 """
-command_names = ("/lemonhelp","/purge","/play <url>") #tuple of command names LEMON users can access, function names are unique
+command_names = set("/lemonhelp","/purge","/resources") #tuple of command names LEMON users can access, function names are unique
 manuals = {
 "FTC": tuple(["https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-1-traditional-events.pdf", "https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-2-traditional.pdf"]),
 "GEAR": tuple(["https://www.youtube.com/watch?v=tXWykG-8_Mw&list=PLJR32U_dICxeLXuzJeBRt3xAuo1TEZHdZ"]),
@@ -55,110 +55,116 @@ async def on_member_remove(member):
 #listen for user messages
 @client.event
 async def on_message(message):
-    #checks for admin priviledge, returns True if user sending message is admin
-    def user_admin_test(message):
-        #obtain a set of the names of the user roles
-        user_roles = set()
-        for role in message.author.roles:
-            user_roles.update(set([role.name])) #sets implemented with hash tables-speeds program up
-        return "Admin" in user_roles
-
-    #checks message for any mentions of lemon, returns true if lemon mentioned
-    def lemon_mention(message):
-        #Determine if LEMON is mentioned via role id or user id
-        mentioned = False
-        for role in message.role_mentions:
-            if 980727200045219894 == role.id:   #lemon's role id
-                mentioned = True
-        for mention in message.mentions:
-            if mention.id == 823964022071754752: #lemon's user id
-                mentioned = True
-        return mentioned
-
-    #checks if the message contains attachments and returns true if it does
-    def has_attachements(message):
-        if len(message.attachments)!=0:
-            return True
-        return False
-
-    if str(message.channel.type) != "private":
-        channel = str(message.channel.name) #get channel name if not a DM
-
-    #gather string reps of information about the message
+    #lowercase the contents of the message
     message.content.lower()
+    #gather string reps of information about the message
     username = str(message.author).split("#")[0] #remove discriminator
     user_message = str(message.content) #get the message sent
-
     # do not repeat events when the bot is message sender
     if message.author == client.user:
         return
 
-    # determine if LEMON is being used for ModMail interactions
-    if str(message.channel.type) == "private":
-        mod_mail_channel = discord.utils.get(client.get_all_channels(), name="mod-mail") #get the mod mail channel from input
-        #check for attachments
-        if has_attachements(message): #this allows submissions of files for analysis
-            await mod_mail_channel.send(f"{username} attached:")
-            for file in message.attachments:
-                await mod_mail_channel.send(file.url)
-            return None
+    #confirm that the message is a valid command before checking command operations
+    if message.content.split()[0] in command_names:
 
-        else:
-            await mod_mail_channel.send(f"{username} submitted: {user_message}")
-            return None
+        #checks for admin priviledge, returns True if user sending message is admin
+        def user_admin_test(message):
+            #obtain a set of the names of the user roles
+            user_roles = set()
+            for role in message.author.roles:
+                user_roles.update(set([role.name])) #sets implemented with hash tables-speeds program up
+            return "Admin" in user_roles
 
-    #determine if msg is a response to modmail
-    elif channel == "mod-mail" and len(message.mentions)!=0:
-        member_obj = message.mentions[0]    #get user mentioned
-        #check for file responses:
-        if has_attachements(message):
-            for file in message.attachments:
-                await member_obj.send(f"Moderator {username} has replied with a file:")
-                await member_obj.send(file.url)
-            return None
-        else:
-            pos = message.content.index(" ")    #get everything after mention
-            mod_message = user_message[pos:]    #modify the string to not have a mention
-            await member_obj.send(f"{username} responded to your mod-mail: {mod_message}")
-            return None
+        #checks message for any mentions of lemon, returns true if lemon mentioned
+        def lemon_mention(message):
+            #Determine if LEMON is mentioned via role id or user id
+            mentioned = False
+            for role in message.role_mentions:
+                if 980727200045219894 == role.id:   #lemon's role id
+                    mentioned = True
+            for mention in message.mentions:
+                if mention.id == 823964022071754752: #lemon's user id
+                    mentioned = True
+            return mentioned
 
-    #if user is sending a greeting, greet the user back with a help cmd
-    if lemon_mention(message):
-        await message.channel.send(f"Salutations {username}. For my help: please type /lemonhelp")
-        return None
+        #checks if the message contains attachments and returns true if it does
+        def has_attachements(message):
+            if len(message.attachments)!=0:
+                return True
+            return False
 
-    #lemonhelp menu
-    if user_message == "/lemonhelp":
-        await message.channel.send(f"Hello {username}, my current supported commands are: {command_names}. You can also PM me to send a ModMail message for the Q/A board")
-        return None
+        if str(message.channel.type) != "private":
+            channel = str(message.channel.name) #get channel name if not a DM
 
-    #online resource urls lookup
-    if user_message.startswith("/resources"):
-        tokens = user_message.split()   #split string at whitespaces
-        #remove the /resources...
-        tokens.pop(0)
-        for token in tokens:
-            token = token.upper()   #uppercase the keys
-            try:
-                accessed = manuals[token]   #access the dictionary at the key if the key exists
-            except:
-                await message.channel.send(f"Sorry {username}, {token} was not a key in our resource table. :(")
+        # determine if LEMON is being used for ModMail interactions
+        if str(message.channel.type) == "private":
+            mod_mail_channel = discord.utils.get(client.get_all_channels(), name="mod-mail") #get the mod mail channel from input
+            #check for attachments
+            if has_attachements(message): #this allows submissions of files for analysis
+                await mod_mail_channel.send(f"{username} attached:")
+                for file in message.attachments:
+                    await mod_mail_channel.send(file.url)
+                return None
+
             else:
-                #access the resource tuple stored at the key
-                for url in accessed:
-                    await message.channel.send(f"{url}")
-        return None
+                await mod_mail_channel.send(f"{username} submitted: {user_message}")
+                return None
 
-    """
-    Below this point is admin-only commands
-    """
-    #admin command to clean current text channel
-    if user_message == "/purge" and user_admin_test(message) and channel != "ddlc-dump":
-        await message.channel.purge(limit=100)
-        print(f"Purging of {channel} for 100 messages completed succsessfully")
-        return None
+        #determine if msg is a response to modmail
+        elif channel == "mod-mail" and len(message.mentions)!=0:
+            member_obj = message.mentions[0]    #get user mentioned
+            #check for file responses:
+            if has_attachements(message):
+                for file in message.attachments:
+                    await member_obj.send(f"Moderator {username} has replied with a file:")
+                    await member_obj.send(file.url)
+                return None
+            else:
+                pos = message.content.index(" ")    #get everything after mention
+                mod_message = user_message[pos:]    #modify the string to not have a mention
+                await member_obj.send(f"{username} responded to your mod-mail: {mod_message}")
+                return None
+
+        #if user is sending a greeting, greet the user back with a help cmd
+        if lemon_mention(message):
+            await message.channel.send(f"Salutations {username}. For my help: please type /lemonhelp")
+            return None
+
+        #lemonhelp menu
+        if user_message == "/lemonhelp":
+            await message.channel.send(f"Hello {username}, my current supported commands are: {command_names}. You can also PM me to send a ModMail message for the Q/A board")
+            return None
+
+        #online resource urls lookup
+        if user_message.startswith("/resources"):
+            tokens = user_message.split()   #split string at whitespaces
+            #remove the /resources...
+            tokens.pop(0)
+            for token in tokens:
+                token = token.upper()   #uppercase the keys
+                try:
+                    accessed = manuals[token]   #access the dictionary at the key if the key exists
+                except:
+                    await message.channel.send(f"Sorry {username}, {token} was not a key in our resource table. :(")
+                else:
+                    #access the resource tuple stored at the key
+                    for url in accessed:
+                        await message.channel.send(f"{url}")
+            return None
+
+        """
+        Below this point is admin-only commands
+        """
+        #admin command to clean current text channel
+        if user_message == "/purge" and user_admin_test(message) and channel != "ddlc-dump":
+            await message.channel.purge(limit=100)
+            print(f"Purging of {channel} for 100 messages completed succsessfully")
+            return None
+        else:
+            await message.channel.send(f"Sorry {username}, you have insufficient permissions to use this command!")
+            return None
+    
     else:
-        await message.channel.send(f"Sorry {username}, you have insufficient permissions to use this command!")
         return None
 
 #notify machine that LEMON has disconnected during runtime
